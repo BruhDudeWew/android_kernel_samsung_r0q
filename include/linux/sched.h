826e7c2aa09cdb2e7dd42780a4548d260686d53a
BruhDudeWew <Me45672@gmail.com>
2026-06-20 15:14:37 +0700

    Droidspaces support
    - Applied 2 patches for Droidspaces support
    - A new defconfig with all Droidspaces options enabled

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -968,8 +968,8 @@ struct task_struct {
 	struct nameidata		*nameidata;
 
 #ifdef CONFIG_SYSVIPC
-	struct sysv_sem			sysvsem;
-	struct sysv_shm			sysvshm;
+	// struct sysv_sem			sysvsem;
+	// struct sysv_shm			sysvshm;
 #endif
 #ifdef CONFIG_DETECT_HUNG_TASK
 	unsigned long			last_switch_count;
@@ -1384,9 +1384,14 @@ struct task_struct {
 	ANDROID_KABI_USE(1, void *pf_io_worker);
 
 	ANDROID_KABI_RESERVE(2);
+#ifdef CONFIG_SYSVIPC
+	ANDROID_KABI_USE(3, struct sysv_sem sysvsem);
+	_ANDROID_KABI_REPLACE(ANDROID_KABI_RESERVE(4); ANDROID_KABI_RESERVE(5), struct sysv_shm sysvshm);
+#else
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
 	ANDROID_KABI_RESERVE(5);
+#endif
 	ANDROID_KABI_RESERVE(6);
 	ANDROID_KABI_RESERVE(7);
 	ANDROID_KABI_RESERVE(8);
